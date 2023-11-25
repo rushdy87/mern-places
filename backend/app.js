abfc8fb8709240fddea511982e23e3e5ac0c4a71
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-11-26 00:36:09 +0300

    Sitting up project, add place route and user place route, and implment Error handling medilware

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const PlacesRoutes = require('./routes/places-routes');
+
+const app = express();
+
+// Regiser routes
+app.use('/api/places', PlacesRoutes);
+
+// Error handling medilware
+// This functon will execute when any middleware in front of it yield an erroe
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || 'An unknown error occurred!' });
+});
+
+const PORT = 3030;
+
+app.listen(PORT, () => console.log(`The server listennin to port ${PORT}...`));
