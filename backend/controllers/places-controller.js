0e2c6ce57d0bcd0d209690ee215189f1a1ff7602
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-11-26 01:58:40 +0300

    Deleting place

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,7 +1,7 @@
 const uuid = require('uuid').v4;
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
@@ -68,7 +68,12 @@ const updatePlace = (req, res, next) => {
   res.status(200).json({ place: updatedPlace });
 };
 
-const deletePlace = (req, res, next) => {};
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+
+  res.status(200).json({ message: 'Deleted place.' });
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
