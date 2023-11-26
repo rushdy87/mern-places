757eaefbe57827bbc6e39cd6d3b10deabe275e9f
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-11-26 23:46:31 +0300

    Validation api input with 'express-validator' middleware

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controller');
 
@@ -6,8 +7,16 @@ const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup', usersController.signup);
+router.post(
+  '/signup',
+  [
+    check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    check('password').isLength({ min: 6 }),
+  ],
+  usersController.signup
+);
 
-router.post('/login', usersController.login);
+router.post('/login', [], usersController.login);
 
 module.exports = router;
