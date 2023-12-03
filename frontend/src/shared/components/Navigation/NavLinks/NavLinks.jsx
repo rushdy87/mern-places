b3f24b9dc226633858d01d98d1393029821c09cb
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-12-04 02:25:17 +0300

    Fixing NavLinks and  My Places link

--- frontend/src/shared/components/Navigation/NavLinks/NavLinks.jsx
+++ frontend/src/shared/components/Navigation/NavLinks/NavLinks.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = (props) => {
-  const { isLoggedIn, logout } = useContext(AuthContext);
+  const { isLoggedIn, logout, userId } = useContext(AuthContext);
 
   return (
     <ul className='nav-links'>
@@ -13,7 +13,7 @@ const NavLinks = (props) => {
       </li>
       {isLoggedIn && (
         <li>
-          <NavLink to='/u1/places'>MY PLACES</NavLink>
+          <NavLink to={`/${userId}/places`}>MY PLACES</NavLink>
         </li>
       )}
       {isLoggedIn && (
