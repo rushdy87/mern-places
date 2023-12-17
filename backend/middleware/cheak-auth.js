ba99efa78aef98ce2cfe556e68c4705dddeebc31
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-12-17 07:20:20 +0300

    Backend Route Protection with Auth Middleware

---
+++ backend/middleware/cheak-auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const HttpError = require('../models/http-error');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]; // authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error('Authentication faild!');
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError('Authentication faild!', 401);
+    return next(error);
+  }
+};
