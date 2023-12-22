d65570dfa89f11d90fad5366e21d00966671c6c1
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-12-22 23:57:55 +0300

    Adding Code Splitting to React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,48 @@
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from 'react-router-dom';
-import { Users, Auth } from './user/pages';
+// import { Users, Auth } from './user/pages';
 import { MainNavigation } from './shared/components/Navigation';
-import { UpdatePlace, UserPlaces, NewPlace } from './places/pages';
+// import { UpdatePlace, UserPlaces, NewPlace } from './places/pages';
 import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks';
+import { LoadingSpinner } from './shared/components/UIElements';
+
+const Users = React.lazy(() => import('./user/pages/Users'));
+const Auth = React.lazy(() => import('./user/pages/Auth/Auth'));
+const UpdatePlace = React.lazy(() =>
+  import('./places/pages').then((module) => ({ default: module.UpdatePlace }))
+);
+const UserPlaces = React.lazy(() =>
+  import('./places/pages').then((module) => ({ default: module.UserPlaces }))
+);
+const NewPlace = React.lazy(() =>
+  import('./places/pages').then((module) => ({ default: module.NewPlace }))
+);
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
-  let routes;
-  if (token) {
-    routes = (
-      <>
-        <Route path='/' element={<Users />} />
-        <Route path='/places/new' element={<NewPlace />} />
-        <Route path='/:userId/places' element={<UserPlaces />} />
-        <Route path='/places/:placeId' element={<UpdatePlace />} />
-        <Route path='*' element={<Navigate to='/' replace />} />
-      </>
-    );
-  } else {
-    routes = (
-      <>
-        <Route path='/' element={<Users />} />
-        <Route path='/:userId/places' element={<UserPlaces />} />
-        <Route path='/auth' element={<Auth />} />
-        <Route path='*' element={<Navigate to='/auth' replace />} />
-      </>
-    );
-  }
+
+  const renderRoutes = token ? (
+    <Routes>
+      <Route path='/' element={<Users />} />
+      <Route path='/places/new' element={<NewPlace />} />
+      <Route path='/:userId/places' element={<UserPlaces />} />
+      <Route path='/places/:placeId' element={<UpdatePlace />} />
+      <Route path='*' element={<Navigate to='/' replace />} />
+    </Routes>
+  ) : (
+    <Routes>
+      <Route path='/' element={<Users />} />
+      <Route path='/:userId/places' element={<UserPlaces />} />
+      <Route path='/auth' element={<Auth />} />
+      <Route path='*' element={<Navigate to='/auth' replace />} />
+    </Routes>
+  );
 
   return (
     <AuthContext.Provider
@@ -41,7 +51,15 @@ const App = () => {
       <Router>
         <MainNavigation />
         <main>
-          <Routes>{routes}</Routes>
+          <Suspense
+            fallback={
+              <div className='center'>
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {renderRoutes}
+          </Suspense>
         </main>
       </Router>
     </AuthContext.Provider>
