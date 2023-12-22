cee04ffa1b3daacb993440b9a509dbabea0238db
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-12-23 01:03:02 +0300

    Deploying a Combined App

--- backend/app.js
+++ backend/app.js
@@ -13,24 +13,29 @@ const app = express();
 app.use(express.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//   next();
+// });
 
 // Regiser routes
 app.use('/api/places', PlacesRoutes);
 app.use('/api/users', UsersRoutes);
 
 app.use((req, res, next) => {
-  return next(new HttpError('Could not find this route.', 404));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+
+// app.use((req, res, next) => {
+//   return next(new HttpError('Could not find this route.', 404));
+// });
 
 // Error handling medilware
 // This functon will execute when any middleware in front of it yield an erroe
